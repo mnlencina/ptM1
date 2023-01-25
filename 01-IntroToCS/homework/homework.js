82889eaa3f9e7bbd5951d0f254377899b563cf3d
mnlencina <mnlencina@gmail.com>
2023-01-25 19:30:03 -0300

    terminado intro

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,21 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
+   let decimal = 0;
+   for (let i = num.length; i > 0 ; i--){
+      decimal += num[i-1] * Math.pow(2,(num.length-i));
+   }
+   return decimal;
+}
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+   let str = '';
+   while (num != 1){
+      str = num % 2 + str;
+      num = Math.floor(num/2)
+   }
+   return num + str;
+}
 
 module.exports = {
    BinarioADecimal,
